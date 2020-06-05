2fe4cca80bd9f6960754f492969fe0f7a62112a8
Jose <jgerena1111@gmail.com>
2020-06-05 11:28:21 -0400

    setting up app

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,5 @@
+//= require jquery
+//= require jquery_ujs
+//= require jquery-fileupload
+//= require_tree .
+//= require jquery-fileupload/jquery-ui
---
+++ db/migrate/20200603184305_create_movies.rb
@@ -0,0 +1,8 @@
+class CreateMovies < ActiveRecord::Migration[6.0]
+  def change
+    create_table :movies do |t|
+      t.string 'title', limit: 255, null: false
+      t.timestamps
+    end
+  end
+end
